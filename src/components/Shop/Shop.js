6cfa42b9fe1b3f1ffb975c90be1e3036c6218106
Junaith1994 <daya2018ctg@gmail.com>
2023-05-03 09:42:48 +0600

    added loading cart items from local storage and show to UI

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,24 +2,50 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-
+    // console.log(cart);
     useEffect(() => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     }, [])
 
+    // Loading local Storage shopping-cart and adding to UI
+    useEffect(() => {
+      const storedCart = getShoppingCart();
+      const savedCart = [];
+      for(const id in storedCart) {
+        const addedProduct = products.find(product => product.id === id);
+        if(addedProduct) {
+          const quantity = storedCart[id];
+          addedProduct.quantity = quantity;
+          savedCart.push(addedProduct);
+        }
+      }
+      setCart(savedCart);
+    }, [products])
+
     // Adding product to the Cart
-    const addToCart = product => {
-      // console.log(product);
-      const newCart = [...cart, product];
+    const addToCart = selectedProduct => {
+      // console.log(selectedProduct);
+      let newCart = [];
+      const existProduct = cart.find(product => product.id === selectedProduct.id);
+      if(!existProduct) {
+        selectedProduct.quantity = 1;
+        newCart = [...cart, selectedProduct]
+      }
+      else {
+        const restProducts = cart.filter(product => product.id !== selectedProduct.id);
+        existProduct.quantity = existProduct.quantity + 1;
+        newCart = [...restProducts, existProduct];
+      }
+      
       setCart(newCart);
-      addToDb(product.id);
+      addToDb(selectedProduct.id);
     }
 
     return (
